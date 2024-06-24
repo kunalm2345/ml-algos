4170ddd36a6b3a8d8771707526b1addc0bca2735
Kunal Mishra <48254970+kunalm2345@users.noreply.github.com>
2024-06-24 23:50:04 +0530

    some error i needa fix

--- logistic_reg.py
+++ logistic_reg.py
@@ -1,20 +1,18 @@
-
 import numpy as np
 
 # Training dataset
-x_train = np.array([1.0, 2.0])
-y_train = np.array([300.0, 500.0]) 
+x_train = np.array([[1.0, 2.0],[2.0, 3.0]])
+y_train = np.array([1, 0]) 
 
 def f_wb(x_train, w, b):
-    y_pred = np.zeros(len(x_train))
+    y_pred = np.zeros(len(x_train)) # same shape as y_train
     for i in range(len(x_train)):
-        y_pred[i] = w * x_train[i] + b
+        y_pred[i] = (1 / (1 + np.exp(np.dot(-w,x_train[i]) - b)))
     return y_pred
 
 def J_wb(x_train, y_train, w, b):
     y_pred = f_wb(x_train, w, b)
-    #print("-->", 0.5 * np.sum((y_train - y_pred) ** 2) * (1 / len(x_train)))
-    return 0.5 * np.sum((y_train - y_pred) ** 2) * (1 / len(x_train))
+    return (1 / len(x_train)) * np.sum(-y_train * np.log(y_pred) - (1 - y_train) * np.log(1 - y_pred))
 
 def dJ_dw(x_train, y_train, w, b):
     return (1/len(x_train)) * np.sum((f_wb(x_train, w, b) - y_train)*x_train)
@@ -28,15 +26,20 @@ def grad_desc(x_train, y_train, w, b, alpha):
         tmp_w = w - alpha * dJ_dw(x_train, y_train, w, b)
         tmp_b = b - alpha * dJ_db(x_train, y_train, w, b)
         if abs(J_wb(x_train, y_train, w, b) - J_wb(x_train, y_train, tmp_w, tmp_b)) < 0.0001:
+            print("- ", tmp_w, tmp_b)
+            w = tmp_w
+            b = tmp_b
             break
-        #print(tmp_w, tmp_b)
+        print("- ", tmp_w, tmp_b)
         w = tmp_w
         b = tmp_b
     return w, b
 
 # initiate w random (w,b) ie (0,0)
-w = 0
+w = np.zeros(x_train.shape[1])
 b = 0
 alpha = 0.01
 w,b = grad_desc(x_train, y_train, w, b, alpha)
-print("w:", w, "\nb:", b) #ans is 200, 100
+print("w:", w, "\nb:", b) 
+
+# y = w1x1 + w2x2 + b
